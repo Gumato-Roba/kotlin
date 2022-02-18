3defea5ffe7c54d4067f6f6cd734e0ab1d64fa2f
Gumato-Roba <gumatorobba@gmail.com>
2022-02-18 22:24:08 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,24 @@
+fun main(){
+    myname()
+    division()
+    addition(8,4,6,10)
+    interestingfact()
+}
+fun myname(){
+    println("hello Gumato")
+}
+fun division(){
+    var j = 23
+    var g = 10
+    var modulus = j % g
+    println(modulus)
+
+}
+fun addition(a:Int , b:Int ,c:Int ,d:Int){
+    var sum = a+b+c+d
+    println(sum)
+}
+fun interestingfact(){
+    var interesting ="i love singing"
+    println(interesting)
+}
